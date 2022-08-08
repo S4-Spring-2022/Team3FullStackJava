f1e18c320d7f871cfe5127827a03aeda726413e2
Stephen Squire <88947471+Genoson@users.noreply.github.com>
2022-08-08 16:49:35 -0230

    Fixing some of my own code too

--- finalsprint/src/main/java/com/team3/rest/UserRepository.java
+++ finalsprint/src/main/java/com/team3/rest/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUserName(@Param("userName") String userName);
-    
+    List<User> findByUserName(@Param("userName") String userName);
+
 }
