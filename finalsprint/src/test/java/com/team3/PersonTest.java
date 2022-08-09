d688db72a9a7840c1e8add9c220644710364f0e5
Michael Oreilly <michael.oreilly@keyin.com>
2022-08-09 09:41:54 -0230

    PersonTest test

--- finalsprint/src/test/java/com/team3/PersonTest.java
+++ finalsprint/src/test/java/com/team3/PersonTest.java
@@ -3,6 +3,8 @@ package com.team3;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.team3.rest.Address;
 import com.team3.rest.Person;
 import com.team3.rest.PersonRepository;
 
@@ -18,23 +21,6 @@ public class PersonTest {
     @ExtendWith(MockitoExtension.class)
     public class PersonRepositoryTest {
         
-        @Mock
-        private PersonRepository personRepository;
-        
-        @Test
-        public void testPersonRepository() {
-            Person person = new Person();
-            person.setId(1L);
-            person.setFirstName("John");
-            person.setFirstName("Doe");
-            person.setEmail("john_doe@email.com");
-            person.setPhone("+1 (709) 777-1234");
-            person.setAddress("123 Main Street, New York, NY 10012");
-
-            Mockito.when(personRepository.findById(1L)).thenReturn(person);
-            Optional<Person> foundPerson = personRepository.findById(1L);
-            assertEquals(person, foundPerson);
-            
-        }
+     
     }
 }
